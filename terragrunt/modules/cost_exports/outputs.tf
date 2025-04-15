3af13ab27925f8e8d918feb190ee0a7b777c1e83
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2025-04-15 16:31:01 +0000

    Removing cost exports since SPNs can 't have billing rights

--- terragrunt/modules/cost_exports/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "billing_account_cost_export_id" {
-  description = "The resource ID of the billing account cost management export."
-  value       = azurerm_billing_account_cost_management_export.this.id
-}
-
-output "billing_account_cost_export_name" {
-  description = "The name of the cost export."
-  value       = azurerm_billing_account_cost_management_export.this.name
-}
