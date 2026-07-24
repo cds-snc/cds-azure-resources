2fff4fcfd255b546f9541660e3e9840654cca225
Adam Andrews <adam.andrews@cds-snc.ca>
2026-07-24 15:57:26 -0400

    Gives RBAC to the AI gateway subscription

--- terragrunt/ai_gateway.tf
+++ terragrunt/ai_gateway.tf
@@ -18,3 +18,10 @@ resource "azuread_application_flexible_federated_identity_credential" "ai_gatewa
   issuer                     = "https://token.actions.githubusercontent.com"
   claims_matching_expression = "claims['sub'] matches 'repo:cds-snc/ai-gateway:*'"
 }
+
+# Contributor role on the target subscription so workflows can create resources
+resource "azurerm_role_assignment" "ai_gateway_contributor_subscription" {
+  scope                = "/subscriptions/c4122b45-f2e3-4873-a7fe-b94c1ad2589f"
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.ai_gateway.object_id
+}
